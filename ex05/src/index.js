1d67a5a364b1a20ff812c51c3813d8a4f70f97b2
jasminapalavra <jasmine.palavra@gmail.com>
2021-03-24 09:40:29 +0100

    Added ex05 src and index.js

---
+++ ex05/src/index.js
@@ -0,0 +1,25 @@
+class Microsoft {
+    constructor(name)
+    this.name = name;
+    
+}
+    get name() {
+        return "{name} is a philantropist!";
+}
+set name()
+class Facebook extends Microsoft {
+    super(name, age);
+}
+    studentAge() {
+    var studentAge = ('${name} is ${age}');
+    return studentAge;
+    }
+
+let Facebook = elliot ("Mark Elliot Zuckerberg", 23);
+console.log(elliot.occupation());
+console.log(studentAge());
+
+module.exports = {
+    Microsoft,
+    Facebook
+}
